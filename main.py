23cff4b0377c733a02f8f8997945b833fe709f52
João Vitor Moreira Passos <moreirapassosj@gmail.com>
2023-03-01 00:40:33 -0300

    up main

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
 import validator as vlt
 
 
-print(vlt.cpf.read_cpf("069.064.063-01"))
+cpf = vlt.cpf.read("06906496301")
+print(cpf)
+cpf.generate()
+cpf.generate()
+cpf.generate()
+
+for i in cpf:
+    print(i)
+    
+print("------------------")
+    
+cnpj = vlt.cnpj.CNPJ()
+print(cnpj)
+cnpj.generate()
+cnpj.generate()
+cnpj.generate()
+for i in cnpj:
+    print(i)
+    
+print("------------------")
+
+credit_card = vlt.bank_card.read("5502099573557190")
+print(credit_card)
+credit_card.generate()
+credit_card.generate()
+credit_card.generate()
+for i in credit_card:
+    print(i)
